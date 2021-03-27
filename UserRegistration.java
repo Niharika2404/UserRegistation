955d35337d21c69d659177d9b6d0dfd27b485efb
Niharika2404 <niharikang24@gmail.com>
2021-03-27 17:26:01 +0530

    Added: First name of the user is added

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,7 +1,21 @@
 package UserRegistration;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registraion");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter the First Name: ");
+		String firstname = scan.nextLine();
+		
+		boolean result = new UserRegistration().validateFirstName(firstname);
+		System.out.println(result);
+	}
+	public boolean validateFirstName(String firstname) {
+		return Pattern.compile("^[A-Z][a-z]{2,}").matcher(firstname).matches();
 	}
 }
