f19ca548f48d06cd9bb76dde1f8372e82c942d38
Niharika2404 <niharikang24@gmail.com>
2021-03-27 17:32:24 +0530

    UserRegistration.java

--- UserRegistration.java
+++ UserRegistration.java
@@ -14,8 +14,18 @@ public class UserRegistration {
 		
 		boolean result = new UserRegistration().validateFirstName(firstname);
 		System.out.println(result);
+		
+		System.out.println("Enter the Last Name: ");
+		String lastname = scan.nextLine();
+		
+		boolean result1 = new UserRegistration().validateLastName(lastname);
+		System.out.println(result1);
 	}
 	public boolean validateFirstName(String firstname) {
 		return Pattern.compile("^[A-Z][a-z]{2,}").matcher(firstname).matches();
+	}
+	
+	public boolean validateLastName(String lastname) {
+		return Pattern.compile("^[A-Z][a-z]{2,}").matcher(lastname).matches();
 	}
 }
