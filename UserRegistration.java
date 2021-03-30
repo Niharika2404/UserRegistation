24fbc4f0065de60f664a85d71b03e408ae643588
Niharika2404 <niharikang24@gmail.com>
2021-03-30 21:01:22 +0530

    Added: Password of the user is added

--- UserRegistration.java
+++ UserRegistration.java
@@ -32,6 +32,12 @@ public class UserRegistration {
 		String phonenumber = scan.nextLine();
 		boolean result3 = new UserRegistration().validatePhoneNumber(phonenumber);
 		System.out.println(result3);
+		
+		//password
+		System.out.println("Enter the Password: ");
+		String password = scan.nextLine();
+		boolean result4 = new UserRegistration().validatePassword(password);
+		System.out.println(result4);
 	}
 	
 	public boolean validateFirstName(String firstname) {
@@ -48,5 +54,9 @@ public class UserRegistration {
 	
 	public boolean validatePhoneNumber(String phonenumber) {
 		return Pattern.compile("^[0-9]{2} [0-9]{10}$").matcher(phonenumber).matches();
+	}
+	
+	public boolean validatePassword(String password) {
+		return Pattern.compile("[a-z0-9]{8,}").matcher(password).matches();
 	}
 }
